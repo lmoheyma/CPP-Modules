f3af04a3f36b758da97cddc43b1a271532cc1c2e
lmoheyma <louismoheymani@gmail.com>
2024-02-19 20:45:47 +0100

    new : ex01 & ex02 files

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmoheyma <lmoheyma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/19 19:05:50 by lmoheyma          #+#    #+#             */
+/*   Updated: 2024/02/19 19:42:09 by lmoheyma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int i;
+	int	j;
+
+	i = 1;
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		while (argv[i])
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				std::cout << (char)toupper(argv[i][j]);
+				j++;
+			}
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
