2a0809a6dfbfd4d186cb9a5bf5a7a2e228e61dad
lmoheyma <louismoheymani@gmail.com>
2024-02-21 06:07:48 +0100

    cpp00 finished

--- ex02/src/Account.cpp
+++ ex02/src/Account.cpp
@@ -6,9 +6,125 @@
 /*   By: lmoheyma <lmoheyma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 02:15:46 by lmoheyma          #+#    #+#             */
-/*   Updated: 2024/02/21 02:15:56 by lmoheyma         ###   ########.fr       */
+/*   Updated: 2024/02/21 05:47:34 by lmoheyma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Account.hpp"
+#include <ctime>
+#include <iostream>
+#include <iomanip>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+void Account::_displayTimestamp(void)
+{
+	std::time_t result = std::time(0);
+	std::cout << std::setfill('0') << "[" << 1900 + std::localtime(&result)->tm_year 
+		<< std::setw(2) << 1 + std::localtime(&result)->tm_mon
+		<< std::setw(2) << std::localtime(&result)->tm_mday << "_"
+		<< std::setw(2) << std::localtime(&result)->tm_hour
+		<< std::setw(2) << std::localtime(&result)->tm_min
+		<< std::setw(2) << std::localtime(&result)->tm_sec
+		<< "] ";
+}
+
+Account::~Account(void)
+{
+	_nbAccounts--;
+	this->_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:";
+	std::cout << _amount << ";closed" << std::endl;
+}
+
+Account::Account(int initial_deposit)
+{
+	_nbAccounts++;
+	_accountIndex = _nbAccounts - 1;
+	_amount = initial_deposit;
+	_totalAmount += _amount;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	this->_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:";
+	std::cout << _amount << ";created" << std::endl;
+}
+
+int	Account::getNbAccounts(void)
+{
+	return (_nbAccounts);
+}
+
+int Account::getTotalAmount(void)
+{
+	return (_totalAmount);
+}
+
+int Account::getNbDeposits(void)
+{
+	return (_totalNbDeposits);
+}
+
+int Account::getNbWithdrawals(void)
+{
+	return (_totalNbWithdrawals);
+}
+
+void	Account::displayAccountsInfos(void)
+{
+	_displayTimestamp();
+	std::cout << "accounts:" << getNbAccounts() << ";";
+	std::cout << "total:" << getTotalAmount() << ";";
+	std::cout << "deposits:" << getNbDeposits() << ";";
+	std::cout << "withdrawals:" << getNbWithdrawals() << ";";
+	std::cout << std::endl;
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	_nbDeposits++;
+	_totalNbDeposits++;
+	_totalAmount += deposit;
+	this->_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";p_amount:" << _amount;
+	_amount += deposit;
+	std::cout << ";deposit:" << deposit << ";amount:" << _amount;
+	std::cout << ";nb_deposits:" << _nbDeposits;
+	std::cout << std::endl;
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	this->_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";p_amount:" << _amount;
+	if (withdrawal > checkAmount())
+	{
+		std::cout << ";withdrawal:refused" << std::endl;
+		return (false);
+	}
+	_nbWithdrawals++;
+	_totalNbWithdrawals++;
+	_totalAmount -= withdrawal;
+	_amount -= withdrawal;
+	std::cout << ";withdrawal:" << withdrawal << ";amount:" << _amount;
+	std::cout << ";nb_withdrawals:" << _nbWithdrawals;
+	std::cout << std::endl;
+	return (true);
+}
+
+int		Account::checkAmount( void ) const
+{
+	return (_amount);
+}
+
+void	Account::displayStatus( void ) const
+{
+	this->_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount;
+	std::cout << ";deposits:" << _nbDeposits << ";withdrawals:" << _nbWithdrawals;
+	std::cout << std::endl;
+}
 
--- ex02/src/tests.cpp
+++ ex02/src/tests.cpp
@@ -37,6 +37,7 @@ int		main( void ) {
 	ints_t::iterator	wit_begin	= withdrawals.begin();
 	ints_t::iterator	wit_end		= withdrawals.end();
 
+	
 	Account::displayAccountsInfos();
 	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
 
