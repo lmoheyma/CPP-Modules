3f89fc5b82b33381a6eb7546994e6387670be03b
lmoheyma <louismoheymani@gmail.com>
2024-03-09 01:21:33 +0100

    ✅: ex01 modif

--- cpp-module-06/ex01/src/main.cpp
+++ cpp-module-06/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lmoheyma <lmoheyma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 23:55:45 by lmoheyma          #+#    #+#             */
-/*   Updated: 2024/03/09 00:13:38 by lmoheyma         ###   ########.fr       */
+/*   Updated: 2024/03/09 00:46:44 by lmoheyma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ int main(void)
     
     data.str = "Message";
     serializedValue = Serializer::serialize(&data);
-    std::cout << "Interpreted value : " << serializedValue << std::endl;
+    std::cout << "\033[1m\033[37mInterpreted value : \033[1m\033[36m" << serializedValue << "\033[0m" << std::endl;
     deserializedValue = Serializer::deserialize(serializedValue);
-    std::cout << "Deserialized value : " << deserializedValue->str << std::endl;
+    std::cout << "\033[1m\033[37mDeserialized value : \033[1m\033[36m" << deserializedValue->str << "\033[0m" << std::endl;
     return (0);
 }
