5ad7d020de92284a5eb4da3f0a5810cf04f7700a
lmoheyma <louismoheymani@gmail.com>
2024-03-09 03:33:19 +0100

    ✅: ex00 main test colors

--- cpp-module-07/ex00/src/main.cpp
+++ cpp-module-07/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lmoheyma <lmoheyma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 02:14:53 by lmoheyma          #+#    #+#             */
-/*   Updated: 2024/03/09 03:01:38 by lmoheyma         ###   ########.fr       */
+/*   Updated: 2024/03/09 03:31:22 by lmoheyma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int main( void ) {
     int b = 3;
     
     ::swap( a, b );
-    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "\033[1m\033[37ma = " << a << ", b = " << b << std::endl;
     std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
     std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
@@ -27,7 +27,7 @@ int main( void ) {
     ::swap(c, d);
     std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << "\033[0m" << std::endl;
     
     return 0;
 }
