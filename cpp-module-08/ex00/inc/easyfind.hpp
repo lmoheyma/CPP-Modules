4c3f1ad617019f35c0dd65885bc6387323a7b6a5
lmoheyma <louismoheymani@gmail.com>
2024-03-11 06:17:32 +0100

    ✅: ex00 done!

--- cpp-module-08/ex00/inc/easyfind.hpp
+++ cpp-module-08/ex00/inc/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: lmoheyma <lmoheyma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 03:40:43 by lmoheyma          #+#    #+#             */
-/*   Updated: 2024/03/11 03:51:07 by lmoheyma         ###   ########.fr       */
+/*   Updated: 2024/03/11 06:14:21 by lmoheyma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,16 @@
 template<typename T>
 int easyfind(T& intContainer, int toFind)
 {
-    for (int it = intContainer.begin(); it != intContainer.end(); it++)
+    for (std::vector<int>::iterator it = intContainer.begin(); it != intContainer.end(); it++)
     {
         if (*it == toFind)
             return (1);
     }
     return (0);
 }
+
+class NotFoundException : public std::exception{
+			public:
+				const char* what() const throw () { return ("\033[1m\033[31mInteger not found!\033[0m");}};
 
 #endif
---
+++ cpp-module-08/ex00/src/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmoheyma <lmoheyma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/11 05:58:10 by lmoheyma          #+#    #+#             */
+/*   Updated: 2024/03/11 06:15:21 by lmoheyma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+
+int main(void)
+{
+    std::vector<int> intVector;
+    std::vector<int>::iterator it;
+    intVector.push_back(13);
+    intVector.push_back(4);
+    intVector.push_back(9);
+    intVector.push_back(1);
+    intVector.push_back(5);
+    try
+    {
+        int found = easyfind(intVector, 13);
+        if (found)
+            std::cout << "\033[1m\033[32mInteger found!\033[0m" << std::endl;
+        else
+            throw NotFoundException();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    return (0);
+}
