8a100d27498b058eed9b014ba8e959fe04e987d6
lmoheyma <louismoheymani@gmail.com>
2024-03-11 06:18:32 +0100

    ✅: indentations!

--- cpp-module-08/ex00/inc/easyfind.hpp
+++ cpp-module-08/ex00/inc/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: lmoheyma <lmoheyma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 03:40:43 by lmoheyma          #+#    #+#             */
-/*   Updated: 2024/03/11 06:14:21 by lmoheyma         ###   ########.fr       */
+/*   Updated: 2024/03/11 06:18:03 by lmoheyma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ int easyfind(T& intContainer, int toFind)
 }
 
 class NotFoundException : public std::exception{
-			public:
-				const char* what() const throw () { return ("\033[1m\033[31mInteger not found!\033[0m");}};
+    public:
+        const char* what() const throw () { return ("\033[1m\033[31mInteger not found!\033[0m");}};
 
 #endif
