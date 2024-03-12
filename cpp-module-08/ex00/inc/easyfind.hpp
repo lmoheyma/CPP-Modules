89a18b73e5cd2da3a42c69b1a9e147f1e96de1d1
lmoheyma <louismoheymani@gmail.com>
2024-03-12 03:20:15 +0100

    ✅: Using of STL Algorithm instead of iterators

--- cpp-module-08/ex00/inc/easyfind.hpp
+++ cpp-module-08/ex00/inc/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: lmoheyma <lmoheyma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 03:40:43 by lmoheyma          #+#    #+#             */
-/*   Updated: 2024/03/11 06:18:03 by lmoheyma         ###   ########.fr       */
+/*   Updated: 2024/03/12 03:08:07 by lmoheyma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,18 @@
 
 #include <iostream>
 #include <vector>
-
-template<typename T>
-int easyfind(T& intContainer, int toFind)
-{
-    for (std::vector<int>::iterator it = intContainer.begin(); it != intContainer.end(); it++)
-    {
-        if (*it == toFind)
-            return (1);
-    }
-    return (0);
-}
+#include <algorithm>
 
 class NotFoundException : public std::exception{
     public:
         const char* what() const throw () { return ("\033[1m\033[31mInteger not found!\033[0m");}};
+
+template<typename T>
+int easyfind(T& intContainer, int toFind)
+{
+    if (std::find(intContainer.begin(), intContainer.end(), toFind) == intContainer.end())
+        return (throw NotFoundException(), 0);
+    return (1);
+}
 
 #endif
